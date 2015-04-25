791e86aaa99dd0f122c12e8268522bd2ec307cbd
Claudio Dettoni <claudio.dettoni@seecrypt.com>
2015-04-25 17:59:41 -0300

    Adicionando contagem do tempo em mean

--- mean.c
+++ mean.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <time.h>
+
+struct timeval begin_tv, end_tv;
+void start() {
+  gettimeofday(&begin_tv, NULL);
+}
+void end() {
+  gettimeofday(&end_tv, NULL);
+}
+double get_sec() {
+  return (double)(end_tv.tv_usec-begin_tv.tv_usec)/1000000 + (double)(end_tv.tv_sec-begin_tv.tv_sec);
+}
+
 
 /**
  * Calcula a média de um array de números reais com o número de processadores especificado
@@ -23,15 +36,19 @@ int main(int argc, char **argv) {
 
   /* inicializacao do array */
   printf("Gerando array com %d números reais...\n", tamanho);
+  start();
   int i;
   for (i = 0; i < tamanho; i++) {
     A[i] = i;
   }
+  end();
   printf("Espaço ocupado por um float: %lu bytes\n", sizeof(float));
-  printf("Espaço total do array: %f MB\n\n", sizeof(float) * tamanho / (1024.0 * 1024.0));
+  printf("Espaço total do array: %f MB\n", sizeof(float) * tamanho / (1024.0 * 1024.0));
+  printf("Tempo para gerar o array: %f s\n\n", get_sec());
 
   omp_set_num_threads(nprocs);
   double soma = 0;
+  start();
   #pragma omp parallel
   {
 
@@ -45,7 +62,9 @@ int main(int argc, char **argv) {
   }
 
   double mean = soma / tamanho;
+  end();
   printf("Média dos valores: %f\n", mean);
+  printf("Tempo para calcular: %f s\n\n", get_sec());
 
   return 0;
 }
